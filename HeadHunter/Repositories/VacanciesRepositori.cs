19fed661b3d12aec0970add226ff4c15bec5abe8
georgiitereshchenko <georgiitereshchenko@outlook.com>
2022-03-30 20:22:16 +0600

    123

--- HeadHunter/Repositories/VacanciesRepositori.cs
+++ HeadHunter/Repositories/VacanciesRepositori.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using HeadHunter.Enums;
+using HeadHunter.Models;
 
 namespace HeadHunter.Repositories
 {
-    internal class VacanciesRepositori
+    public class VacanciesRepository
     {
+        public List<Vacancy> GetVacancyOpen()
+        {
+            List<Vacancy> vacancies = new List<Vacancy>();
+            using (ApplicationDbContext dbContext = new ApplicationDbContext())
+            {
+                vacancies = dbContext.Vacancies.Where(x => x.Type == VacancyType.Open).ToList();
+            }
+
+            return vacancies;
+        }
+    
     }
 }
