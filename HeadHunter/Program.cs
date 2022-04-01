05c56cdd118c69653b231236d889c25d68636367
Aigerim <ajgerimkubanycbekova2@gamil.com>
2022-04-01 19:01:35 +0300

    регистрация

--- HeadHunter/Program.cs
+++ HeadHunter/Program.cs
@@ -48,7 +48,7 @@ namespace HeadHunter
             Console.WriteLine("Введите адрес компании:");
             address = Console.ReadLine();
             Console.WriteLine("Введите год основания компании:");
-            int.TryParse(Console.ReadLine(), out foundationYear);
+           int.TryParse(Console.ReadLine(), out foundationYear);
 
 
             Employer employer = new Employer()
@@ -90,7 +90,7 @@ namespace HeadHunter
         public static void RegistrationEmployee()
         {
             EmployeeServes _userService = new EmployeeServes();
-
+          
             string firstName, surName, login, password, password2,email, sex;
 
             Console.Clear();
@@ -112,7 +112,7 @@ namespace HeadHunter
 
 
             Employee employee = new Employee()
-            {
+           {
                 Login = login,
                 Password = password,
                 FirstName = firstName,
@@ -139,13 +139,14 @@ namespace HeadHunter
             }
         }
 
+
         public static void AvtorizaionUsser()
         {
             AvtorizaionUsserService _avtorizaion = new AvtorizaionUsserService();
             string login, pass;
             Console.Clear();
             Console.WriteLine("Введите логин");
-            login = Console.ReadLine();
+           login = Console.ReadLine();
             Console.WriteLine("Введите пороль");
             pass = Console.ReadLine();
 
@@ -187,7 +188,7 @@ namespace HeadHunter
                 {
                     if (index + 1 < options.Count)
                     {
-                        index++;
+                       index++;
                         WriteMenu(options, options[index]);
                     }
                 }
@@ -195,8 +196,8 @@ namespace HeadHunter
                 {
                     if (index - 1 >= 0)
                     {
-                        index--;
-                        WriteMenu(options, options[index]);
+                       index--;
+                       WriteMenu(options, options[index]);
                     }
                 }
                 if (keyinfo.Key == ConsoleKey.Enter)
@@ -209,19 +210,19 @@ namespace HeadHunter
 
             Console.ReadKey();
         }
-
+        
         static void WriteMenu(List<Option> options, Option selectedOption)
         {
             Console.Clear();
             foreach (Option option in options)
-            {
-                if (option == selectedOption)
+            { 
+               if (option == selectedOption)
                 {
-                    Console.Write("> ");
+                   Console.Write("> ");
                 }
-                else
+               else
                 {
-                    Console.Write(" ");
+                   Console.Write(" ");
                 }
                 Console.WriteLine(option.Name);
             }
@@ -241,14 +242,79 @@ namespace HeadHunter
         /// </summary>
         public static void RegistrationMenu()
         {
-            Console.Clear();
-            List<Option> Registration = new List<Option>
+           Console.Clear();
+          List<Option> Registration = new List<Option>
             {
-                new Option("Зарегистрироваться как Работодатель", () => RegistrationEmployer() ),
+               new Option("Зарегистрироваться как Работодатель", () => RegistrationEmployer() ),
                 new Option("Зарегистрироваться как Соискатель", () => RegistrationEmployee() )
             };
             Menu(Registration);
         }
+        /// <summary>
+        /// Регистрация вакансий
+        /// </summary>
+        public static void VacancyRegistration()
+        {
+            string name, description, keyskills, address, contact;
+            int experience; int type;
+            decimal salary;
+            int hastest ;
+            DateTime publishedAt = DateTime.Today;
+
+            Console.Clear();
+            Console.WriteLine("Введите название вакансии:");
+            name = Console.ReadLine();
+            Console.WriteLine("Введите описание вакансии:");
+            description = Console.ReadLine();
+            Console.WriteLine("Введите тип вакансии:");
+            int.TryParse(Console.ReadLine(), out type);
+            Console.WriteLine("Введите  ключевые умения:");
+            keyskills = Console.ReadLine();
+            Console.WriteLine("Введите адрес: ");
+            address = Console.ReadLine();
+            Console.WriteLine("Введите опыт работы");
+            int.TryParse(Console.ReadLine(), out experience);
+            Console.WriteLine("Введите зарплату сотрудника: ");
+            decimal.TryParse(Console.ReadLine(), out salary);
+            Console.WriteLine("Введите дату публикации вакансии: ");
+            DateTime.TryParse(Console.ReadLine(), out publishedAt);
+            Console.WriteLine("Введите контакты для связи: ");
+            contact = Console.ReadLine();
+            Console.WriteLine("Будет ли иметься тестовое задание:\n" +
+                                "1: Да\n" +
+                                "2: Нет");
+            int.TryParse(Console.ReadLine(), out hastest);
+
+            Vacancy vacancy = new Vacancy()
+            {
+                Name = name,
+                Description = description,
+                Type = type == 1 ? VacancyType.Open : VacancyType.Closed,
+                KeySkills = keyskills,
+                Address = address,
+                Experience = experience == 1 ? ExperienceType.NoExperience
+                    : experience == 2 ? ExperienceType.Between1And3
+                    : experience == 3 ? ExperienceType.Between3And6
+                    : ExperienceType.MoreThan6,
+                Salary = salary,
+                PublishedAt = publishedAt,
+                Contact = contact,
+              
+            };
+        }
+
+        
+
+
+
+
+
+
+
+
+
+
+    }
         
     }
 }
